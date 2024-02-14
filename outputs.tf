4b91be90b4f0b4f1f69e4db96d872b7f6eb926e8
VaninaDim <57396823+VaninaDim@users.noreply.github.com>
2024-02-14 22:37:45 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.task2webappvd.default_hostname
-}
-
-output "webapp_ips" {
-  value = azurerm_linux_web_app.task2webappvd.outbound_ip_addresses
-}
