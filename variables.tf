f6ec47d6e1f2173f95d7dd671575ff7141dfc896
VaninaDim <57396823+VaninaDim@users.noreply.github.com>
2024-02-14 23:10:16 +0200

    formatted files

--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "resource group name in Azure"
 }
 
 variable "resource_group_location" {
-  type = string
+  type        = string
   description = "resource group location in Azure"
 }
 
 variable "app_service_plan_name" {
-  type = string
+  type        = string
   description = "app service plan name in Azure"
 }
 
 variable "app_service_name" {
-  type = string
+  type        = string
   description = "app service name in Azure"
 }
 
 variable "sql_server_name" {
-  type = string
+  type        = string
   description = "sql server name in Azure"
 }
 
 variable "sql_database_name" {
-  type = string
+  type        = string
   description = "sql db name in Azure"
 }
 
 variable "sql_admin_login" {
-  type = string
+  type        = string
   description = "sql server admin login in Azure"
 }
 
 variable "sql_admin_password" {
-  type = string
+  type        = string
   description = "password for the sql server admin login in Azure"
 }
 
 variable "firewall_rule_name" {
-  type = string
+  type        = string
   description = "firewall rule name in Azure"
 }
 
 variable "repo_URL" {
-  type = string
+  type        = string
   description = "URL to the Github repo"
 }
